2c257cf2144b79e4777f9d6fc63233f7d25b9649
Evgeny Saveliev <e.s.saveliev@gmail.com>
2022-10-20 16:41:51 +0100

    Linter comments

--- data_processing/data_loader.py
+++ data_processing/data_loader.py
@@ -14,22 +14,22 @@ def sample_source_and_target_datasets(
 ):
     np.random.seed(seed)
 
-    if num_examples_target == None:
+    if num_examples_target == None:  # noqa
         num_examples_target = np.random.randint(100, 500)
 
-    if num_examples_source == None:
+    if num_examples_source == None:  # noqa
         num_examples_source = np.random.randint(1000, X.shape[0] - num_examples_target)
 
-    if num_features_shared == None:
+    if num_features_shared == None:  # noqa
         num_features_shared = np.random.randint(5, int(X.shape[1] / 3))
         num_features_source = np.random.randint(5, int(X.shape[1] / 3))
         num_features_target = np.random.randint(5, int(X.shape[1] / 3))
-    elif num_features_shared != None:
+    elif num_features_shared != None:  # noqa
         num_features_source = np.random.randint(1, int((X.shape[1] - num_features_shared) / 2))
         num_features_target = np.random.randint(1, int((X.shape[1] - num_features_shared) / 2))
 
     assert num_examples_source + num_examples_target < X.shape[0]
-    assert num_features_source + num_features_shared + num_features_target < X.shape[1]
+    assert num_features_source + num_features_shared + num_features_target < X.shape[1]  # type: ignore
 
     all_indices_features = np.array(range(X.shape[1]))
     np.random.shuffle(all_indices_features)
@@ -40,17 +40,17 @@ def sample_source_and_target_datasets(
     X_source_specific = X[all_indices_examples[:num_examples_source]][:, all_indices_features[:num_features_source]]
 
     X_source_shared = X[all_indices_examples[:num_examples_source]][
-        :, all_indices_features[num_features_source : num_features_source + num_features_shared]
+        :, all_indices_features[num_features_source : num_features_source + num_features_shared]  # type: ignore
     ]
 
     X_target_shared = X[all_indices_examples[num_examples_source : num_examples_source + num_examples_target]][
-        :, all_indices_features[num_features_source : num_features_source + num_features_shared]
+        :, all_indices_features[num_features_source : num_features_source + num_features_shared]  # type: ignore
     ]
 
     X_target_specific = X[all_indices_examples[num_examples_source : num_examples_source + num_examples_target]][
         :,
         all_indices_features[
-            num_features_source + num_features_shared : num_features_source + num_features_shared + num_features_target
+            num_features_source + num_features_shared : num_features_source + num_features_shared + num_features_target  # type: ignore
         ],
     ]
 
@@ -85,20 +85,20 @@ def load(
         print("Unknown dataset " + str(dataset_name))
 
     if train_ratio == 1.0:
-        return (X_source_specific, X_source_shared, X_target_specific, X_target_shared)
+        return (X_source_specific, X_source_shared, X_target_specific, X_target_shared)  # type: ignore
     else:
 
-        X_source_specific_train = X_source_specific[: int(train_ratio * X_source_specific.shape[0])]
-        X_source_specific_test = X_source_specific[int(train_ratio * X_source_specific.shape[0]) :]
+        X_source_specific_train = X_source_specific[: int(train_ratio * X_source_specific.shape[0])]  # type: ignore
+        X_source_specific_test = X_source_specific[int(train_ratio * X_source_specific.shape[0]) :]  # type: ignore
 
-        X_source_shared_train = X_source_shared[: int(train_ratio * X_source_shared.shape[0])]
-        X_source_shared_test = X_source_shared[int(train_ratio * X_source_shared.shape[0]) :]
+        X_source_shared_train = X_source_shared[: int(train_ratio * X_source_shared.shape[0])]  # type: ignore
+        X_source_shared_test = X_source_shared[int(train_ratio * X_source_shared.shape[0]) :]  # type: ignore
 
-        X_target_specific_train = X_target_specific[: int(train_ratio * X_target_specific.shape[0])]
-        X_target_specific_test = X_target_specific[int(train_ratio * X_target_specific.shape[0]) :]
+        X_target_specific_train = X_target_specific[: int(train_ratio * X_target_specific.shape[0])]  # type: ignore
+        X_target_specific_test = X_target_specific[int(train_ratio * X_target_specific.shape[0]) :]  # type: ignore
 
-        X_target_shared_train = X_target_shared[: int(train_ratio * X_target_shared.shape[0])]
-        X_target_shared_test = X_target_shared[int(train_ratio * X_target_shared.shape[0]) :]
+        X_target_shared_train = X_target_shared[: int(train_ratio * X_target_shared.shape[0])]  # type: ignore
+        X_target_shared_test = X_target_shared[int(train_ratio * X_target_shared.shape[0]) :]  # type: ignore
 
         return (X_source_specific_train, X_source_shared_train, X_target_specific_train, X_target_shared_train), (
             X_source_specific_test,
